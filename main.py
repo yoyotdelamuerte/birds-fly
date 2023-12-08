5b660eaf4f71e62bc223e89cee1f8f9242953851
yoyotdelamuerte <108859385+yoyotdelamuerte@users.noreply.github.com>
2023-12-08 17:28:38 +0100

    Update main.py
    -Add bird on mouse click
    -Change reset position

--- main.py
+++ main.py
@@ -5,48 +5,36 @@ import math
 
 pygame.init()
 
-width, height = 800, 600
+width, height = 1000, 700
 bg_color = (255, 255, 255)
-bird_radius = 5
-num_birds = 50
+bird_radius = 3
+num_birds = 100
 speed_limit = 5
 cohesion_factor = 0.01
 alignment_factor = 0.1
 separation_factor = 10
+avoidance_distance = 100
 
 
 class Bird:
     def __init__(self, x, y):
-        """
-        Initialize a bird with random initial position and angle.
-
-        Parameters:
-        - x: Initial x-coordinate of the bird.
-        - y: Initial y-coordinate of the bird.
-        """
         self.x = x
         self.y = y
         self.angle = random.uniform(0, 2 * math.pi)
         self.color = (207, random.randint(110, 175), random.randint(20, 100))
         self.speed = random.uniform(1, speed_limit)
 
-    def move(self, birds):
-        """
-        Move the bird based on the rules of cohesion, alignment, and separation.
 
-        Parameters:
-        - birds: List of all birds in the simulation.
-        """
-        # Cohesion
+    def move(self, birds):
+
         avg_pos = [sum(b.x for b in birds) / len(birds), sum(b.y for b in birds) / len(birds)]
         angle_to_avg = math.atan2(avg_pos[1] - self.y, avg_pos[0] - self.x)
         self.angle += (angle_to_avg - self.angle) * cohesion_factor
 
-        # Alignment
-        avg_angle = sum(b.angle for b in birds) / len(birds)
-        self.angle += (avg_angle - self.angle) * alignment_factor
+        avg_angle_left = sum(b.angle for b in birds if b.angle < 0) / len(birds)
+        avg_angle_right = sum(b.angle for b in birds if b.angle >= 0) / len(birds)
+        self.angle += (avg_angle_left + avg_angle_right - self.angle) * alignment_factor
 
-        # Separation
         for bird in birds:
             distance = math.sqrt((self.x - bird.x) ** 2 + (self.y - bird.y) ** 2)
             if distance < separation_factor and distance > 0:
@@ -55,10 +43,9 @@ class Bird:
 
         speed_variation = random.uniform(-0.1, 0.1)
         self.speed = max(0.1, min(speed_limit, self.speed + speed_variation))
+
         self.angle += random.uniform(-0.1, 0.1)
-        # Speed limit
         self.angle = max(-speed_limit, min(speed_limit, self.angle))
-        # Update position
         self.x += math.cos(self.angle) * speed_limit
         self.y += math.sin(self.angle) * speed_limit
 
@@ -69,10 +56,14 @@ pygame.display.set_caption("Starling Flocking Simulation")
 birds = [Bird(random.uniform(height / 2, width / 2), random.uniform(width / 2, height / 2)) for _ in range(num_birds)]
 
 
+def add_bird(x, y):
+    birds.append(Bird(x, y))
+
+
 def reset_positions(birds):
     for bird in birds:
-        bird.x = random.uniform(0, width)
-        bird.y = random.uniform(0, height)
+        bird.x = width / 2
+        bird.y = height / 2
 
 
 while True:
@@ -82,11 +73,13 @@ while True:
             sys.exit()
         if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
             reset_positions(birds)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                add_bird(*event.pos)
 
     for bird in birds:
         bird.move(birds)
 
-        # Wrap around the screen
         bird.x = (bird.x + width) % width
         bird.y = (bird.y + height) % height
 
