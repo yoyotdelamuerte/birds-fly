39daad4551e44a1fdfb05f4e0cbe174734dbe424
yoyotdelamuerte <108859385+yoyotdelamuerte@users.noreply.github.com>
2023-12-08 16:30:07 +0100

    Update main.py
    -Add possibility to reset bird position
    -Add Variation color for each bird
    -Add angle variation for each bird
    -Add speed variation for each bird
    -Upgrade pygame fps

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ pygame.init()
 
 width, height = 800, 600
 bg_color = (255, 255, 255)
-bird_color = (0, 0, 0)
 bird_radius = 5
 num_birds = 50
 speed_limit = 5
 cohesion_factor = 0.01
 alignment_factor = 0.1
 separation_factor = 10
+
 
 class Bird:
     def __init__(self, x, y):
@@ -27,6 +27,8 @@ class Bird:
         self.x = x
         self.y = y
         self.angle = random.uniform(0, 2 * math.pi)
+        self.color = (207, random.randint(110, 175), random.randint(20, 100))
+        self.speed = random.uniform(1, speed_limit)
 
     def move(self, birds):
         """
@@ -46,28 +48,40 @@ class Bird:
 
         # Separation
         for bird in birds:
-            distance = math.sqrt((self.x - bird.x)**2 + (self.y - bird.y)**2)
+            distance = math.sqrt((self.x - bird.x) ** 2 + (self.y - bird.y) ** 2)
             if distance < separation_factor and distance > 0:
                 angle_away = math.atan2(self.y - bird.y, self.x - bird.x)
                 self.angle += (angle_away - self.angle) * (1 / distance)
 
+        speed_variation = random.uniform(-0.1, 0.1)
+        self.speed = max(0.1, min(speed_limit, self.speed + speed_variation))
+        self.angle += random.uniform(-0.1, 0.1)
         # Speed limit
         self.angle = max(-speed_limit, min(speed_limit, self.angle))
-
         # Update position
         self.x += math.cos(self.angle) * speed_limit
         self.y += math.sin(self.angle) * speed_limit
 
+
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Starling Flocking Simulation")
 
-birds = [Bird(random.uniform(height/2, width/2), random.uniform(width/2, height/2)) for _ in range(num_birds)]
+birds = [Bird(random.uniform(height / 2, width / 2), random.uniform(width / 2, height / 2)) for _ in range(num_birds)]
+
+
+def reset_positions(birds):
+    for bird in birds:
+        bird.x = random.uniform(0, width)
+        bird.y = random.uniform(0, height)
+
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            reset_positions(birds)
 
     for bird in birds:
         bird.move(birds)
@@ -78,7 +92,7 @@ while True:
 
     screen.fill(bg_color)
     for bird in birds:
-        pygame.draw.circle(screen, bird_color, (int(bird.x), int(bird.y)), bird_radius)
+        pygame.draw.circle(screen, bird.color, (int(bird.x), int(bird.y)), bird_radius)
 
     pygame.display.flip()
-    pygame.time.Clock().tick(30)
+    pygame.time.Clock().tick(60)
